2b5f2ae693db32b898d7bb540749cbbd32f7f557
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-05-05 11:38:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const line=document.getElementByid("line");
+let angle=0;
+set interval(()=>{
+	angle +2=0;
+	line.style.transform=`translate(-50%, -50%) rotate(${angle}deg)`;
+}, 20);
+}
