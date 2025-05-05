795ac0c566e7393e0e85618790b6feb715d216eb
Tushar-Mehra1707 <tusharmehra4321@gmail.com>
2025-05-05 11:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 //your JS code here. If required.
 const line = document.getElementById('line');
-
-// Initialize the rotation angle
 let angle = 0;
-
-// Function to rotate the line
 function rotateLine() {
-    angle += 2; // Increase the angle by 2 degrees
-    line.style.transform = `rotate(${angle}deg)`; // Apply the rotation
+    angle += 2; 
+    line.style.transform =`rotate(${angle}deg)`; 
 }
-
-// Set an interval to rotate the line every 20 milliseconds
 setInterval(rotateLine, 20);
